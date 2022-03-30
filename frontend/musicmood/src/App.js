5f996771387635d5a46a0476fe14c512b0988af0
krshnknsr21 <krshnknsr21@gmail.com>
2022-03-30 11:48:00 +0530

    First commit

---
+++ frontend/musicmood/src/App.js
@@ -0,0 +1,15 @@
+// import { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./pages/Form.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <span>{mood}</span> */}
+      <h1>AudioLytic: Mood Based Song Recommendation ML model</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
